f3d40e26a87b22c36ffc7aa8eab90cec99d710b2
Ommehta27 <om.niftyprojects@gmail.com>
2025-11-10 12:37:29 +0530

    added the signup

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("17e7d45ce2860f25")
+/******/ 		__webpack_require__.h = () => ("0235f59c17db899c")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a28f2f392d5a8344"; }
+/******/ 		__webpack_require__.h = function() { return "ac71657029fc26aa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
